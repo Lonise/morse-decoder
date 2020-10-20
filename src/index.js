fb72ffd191b265fae75fec752ddece1b7dcf48c5
Loshkarev Nikita <loshkarev.n@inbox.ru>
2020-10-20 16:44:02 +0400

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let decodeMessage = '',
+        morseSymbol,
+        encodeSymbol;
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+        encodeSymbol = expr.slice(i, i + 10);
+        morseSymbol = '';
+        if (encodeSymbol === '**********') {
+            decodeMessage += ' '; 
+            continue;
+        }
+        else
+            for (let j = 0; j < encodeSymbol.length; j = j + 2) {
+                switch (encodeSymbol.slice(j, j + 2)) {
+                    case '00': break;
+                    case '10': 
+                        morseSymbol += '.'
+                    break;
+                    case '11': 
+                        morseSymbol += '-'
+                    break;
+                }
+            }
+            decodeMessage += MORSE_TABLE[morseSymbol];
+    }
+    return decodeMessage;
 }
 
 module.exports = {
